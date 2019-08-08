d1fe5d16e1ada5111d896e241ecfe9eecfad22bb
sadjy <sadjy@iota.org>
2019-08-08 11:36:09 -0400

    iri-release-dev fix

--- iri-release/pipeline.sh
+++ iri-release/pipeline.sh
@@ -15,10 +15,11 @@ release () {
   echo "    commands:
       - mkdir -p target
       - apt -qq update && apt -qq install curl -y
-      - VERSION=\$(echo $1 | awk -F- '{print \\\$1}' | tr -d 'v')
-      - curl https://iotaledger-iri-release.s3.eu-central-1.amazonaws.com/\\\$VERSION/iri-\\\$VERSION.jar --output target/iri-\\\$VERSION.jar
-      - curl https://iotaledger-iri-release.s3.eu-central-1.amazonaws.com/\\\$VERSION/SHA256SUM-\\\$VERSION --output target/SHA256SUM-\\\$VERSION
-      - if [[ \\\$(sha256sum target/iri-\\\$VERSION.jar | cut -d \" \" -f 1) == \\\$(cat target/SHA256SUM-\\\$VERSION) ]]; then echo 'CHECKSUM OK'; else exit 1; fi
+      - IRI_VERSION=\$(echo $1 | tr -d 'v')
+      - IRI_VERSION_NUMBER=\$(echo $1 | awk -F- '{print \\\$1}' | tr -d 'v')
+      - curl https://iotaledger-iri-release.s3.eu-central-1.amazonaws.com/\\\$IRI_VERSION_NUMBER/iri-\\\$IRI_VERSION.jar --output target/iri-\\\$IRI_VERSION.jar
+      - curl https://iotaledger-iri-release.s3.eu-central-1.amazonaws.com/\\\$IRI_VERSION_NUMBER/SHA256SUM-\\\$IRI_VERSION --output target/SHA256SUM-\\\$IRI_VERSION
+      - if [[ \\\$(sha256sum target/iri-\\\$IRI_VERSION.jar | cut -d \" \" -f 1) == \\\$(cat target/SHA256SUM-\\\$IRI_VERSION) ]]; then echo 'CHECKSUM OK'; else exit 1; fi
       - curl -L https://github.com/buildkite/github-release/releases/download/v1.0/github-release-linux-amd64 -o github-release
       - chmod +x github-release
       #- gpg --armor --detach-sign --clearsign --default-key email@iota.org target/SHA256SUM
