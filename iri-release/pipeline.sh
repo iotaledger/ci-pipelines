3901717db63fc343d0c50748ba669bd61ff464c0
sadjy <sadjy@iota.org>
2019-10-04 15:17:14 +0900

    bee-/prs: ssl fix. iri-release: prod pipeline update

--- iri-release/pipeline.sh
+++ iri-release/pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -eu
+set -eux
 
 skip_build () {
   echo "  - label: \"Something went wrong, skipping build\""
@@ -14,15 +14,17 @@ release () {
   echo "  - label: \"Releasing - $1\""
   echo "    commands:
       - mkdir -p target
-      - apt -qq update && apt -qq install curl -y
+      - apt -qq update && apt -qq install curl gnupg -y
       - IRI_VERSION=\$(echo $1 | tr -d 'v')
       - IRI_VERSION_NUMBER=\$(echo \\\$IRI_VERSION | awk -F- '{print \\\$1}')
       - curl https://iotaledger-iri-release.s3.eu-central-1.amazonaws.com/\\\$IRI_VERSION_NUMBER/iri-\\\$IRI_VERSION.jar --output target/iri-\\\$IRI_VERSION.jar
       - curl https://iotaledger-iri-release.s3.eu-central-1.amazonaws.com/\\\$IRI_VERSION_NUMBER/SHA256SUM-\\\$IRI_VERSION --output target/SHA256SUM-\\\$IRI_VERSION
       - if [[ \\\$(sha256sum target/iri-\\\$IRI_VERSION.jar | cut -d \" \" -f 1) == \\\$(cat target/SHA256SUM-\\\$IRI_VERSION) ]]; then echo 'CHECKSUM OK'; else exit 1; fi
       - curl -L https://github.com/buildkite/github-release/releases/download/v1.0/github-release-linux-amd64 -o github-release
-      - chmod +x github-release
-      #- gpg --armor --detach-sign --clearsign --default-key email@iota.org target/SHA256SUM
+      - chmod +x github-release      
+      - echo \\\$GPG_KEY | base64 -d > iri.key
+      - echo \\\$GPG_CONTACT_PASSPHRASE | gpg --batch --yes --import <iri.key
+      - echo \\\$GPG_CONTACT_PASSPHRASE | gpg --pinentry-mode loopback --batch --passphrase-fd 0 --armor --detach-sign --default-key contact@iota.org target/SHA256SUM-\\\$IRI_VERSION
       - ./github-release \\\$GITHUB_RELEASE_TAG target/*"
   echo "    plugins:
       https://github.com/iotaledger/docker-buildkite-plugin#release-v3.2.0:
@@ -34,7 +36,9 @@ release () {
           - GITHUB_RELEASE_TAG=$1
           - GITHUB_RELEASE_ACCESS_TOKEN
           - GITHUB_RELEASE_REPOSITORY=iotaledger/iri
-          - GITHUB_RELEASE_COMMIT"
+          - GITHUB_RELEASE_COMMIT
+          - GPG_CONTACT_PASSPHRASE
+          - GPG_KEY"
   echo "    agents:
       queue: aws-m5large"
 }
