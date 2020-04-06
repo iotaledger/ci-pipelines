ba7b60c251d1a61ee337d4eed83691178aed394f
sadjy <sadjy@iota.org>
2020-04-06 21:49:14 +0900

    tests -dev

--- iri-release-dev/pipeline.sh
+++ iri-release-dev/pipeline.sh
@@ -33,8 +33,7 @@ getMilestone () {
   echo "  - label: \"Getting milestone\""
   echo "    commands:
       - rm -rf /cache/*
-      - apt -qq update && apt -qq install curl jq
-      - curl -s http://localhost:14265 -X POST -H 'Content-Type: application/json' -H 'X-IOTA-API-Version: 1' -d '' | jq -r '.latestMilestoneIndex' > /cache/milestone.txt"
+      - apt -qq update && apt -qq install curl jq"
   echo "    plugins:
       https://github.com/iotaledger/docker-buildkite-plugin#release-v3.2.0:
         image: \"iotaledger/iri-dev:$1\"
