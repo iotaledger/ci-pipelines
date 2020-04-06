7dcff5bc222788bab892b3afcba85656f3a4c87b
sadjy <sadjy@iota.org>
2020-04-06 20:25:44 +0900

    tests -dev

--- iri-release-dev/pipeline.sh
+++ iri-release-dev/pipeline.sh
@@ -10,22 +10,61 @@ skip_build () {
       queue: aws-m5large"
 }
 
+getMilestone () {
+  echo "  - label: \"Preparing DB\""
+  echo "    commands:
+      - rm -rf /cache/*
+      - apt -qq update && apt -qq install curl
+      - curl -s https://iotaledger-dbfiles-public.s3.eu-central-1.amazonaws.com/mainnet/iri/latest-LS.tar --output /cache/local-snapshot.tar
+      - curl -s https://iotaledger-dbfiles-public.s3.eu-central-1.amazonaws.com/mainnet/iri/latest-LS.tar.sum --output /cache/local-snapshot.tar.sum
+      - if [[ \\\$(sha256sum target/local-snapshot.tar | cut -d \" \" -f 1) == \\\$(cat target/local-snapshot.tar.sum) ]]; then echo 'CHECKSUM OK'; else exit 1; fi
+      - cd /cache && tar xf local-snapshot.tar"
+  echo "    plugins:
+      https://github.com/iotaledger/docker-buildkite-plugin#release-v3.2.0:
+        image: \"debian\"
+        always-pull: true
+        mount-buildkite-agent: false
+        shell: [\"/bin/bash\", \"-e\", \"-c\"]
+        volumes:
+          - /cache-iri-release-$BUILDKITE_BUILD_ID:/cache"
+  echo "    agents:
+      queue: aws-m5large"
+
+  echo "  - label: \"Getting milestone\""
+  echo "    commands:
+      - rm -rf /cache/*
+      - apt -qq update && apt -qq install curl jq
+      - curl -s http://localhost:14265 -X POST -H 'Content-Type: application/json' -H 'X-IOTA-API-Version: 1' -d '{\"command\": \"getNodeInfo\"}' | jq -r '.latestMilestoneIndex' > /cache/milestone.txt"
+  echo "    plugins:
+      https://github.com/iotaledger/docker-buildkite-plugin#release-v3.2.0:
+        image: \"iotaledger/iri-dev:$1\"
+        always-pull: true
+        mount-buildkite-agent: false
+        shell: [\"/bin/bash\", \"-e\", \"-c\"]
+        volumes:
+          - /cache-iri-release-$BUILDKITE_BUILD_ID:/cache"
+  echo "    agents:
+      queue: aws-m5large"
+}
+
 release () {
   echo "  - label: \"Releasing - $1\""
   echo "    commands:
       - mkdir -p target
       - apt -qq update && apt -qq install curl gnupg -y
-      - IRI_VERSION=\$(echo $1 | tr -d 'v')
-      - IRI_VERSION_NUMBER=\$(echo \\\$IRI_VERSION | awk -F- '{print \\\$1}')
-      - curl https://iri-release-dev.s3.eu-central-1.amazonaws.com/\\\$IRI_VERSION_NUMBER/iri-\\\$IRI_VERSION.jar --output target/iri-\\\$IRI_VERSION.jar
-      - curl https://iri-release-dev.s3.eu-central-1.amazonaws.com/\\\$IRI_VERSION_NUMBER/SHA256SUM-\\\$IRI_VERSION --output target/SHA256SUM-\\\$IRI_VERSION
-      - curl https://iotaledger-dbfiles-public.s3.eu-central-1.amazonaws.com/mainnet/iri/latest-LS.tar --output target/LS.tar
-      - if [[ \\\$(sha256sum target/iri-\\\$IRI_VERSION.jar | cut -d \" \" -f 1) == \\\$(cat target/SHA256SUM-\\\$IRI_VERSION) ]]; then echo 'CHECKSUM OK'; else exit 1; fi
+      - IRI_VERSION_NUMBER=\$(echo $1 | awk -F- '{print \\\$1}')
+      - curl -s https://iri-release-dev.s3.eu-central-1.amazonaws.com/\\\$IRI_VERSION_NUMBER/iri-\\\$IRI_VERSION.jar --output target/IRI-$1.jar
+      - curl -s https://iri-release-dev.s3.eu-central-1.amazonaws.com/\\\$IRI_VERSION_NUMBER/SHA256SUM-\\\$IRI_VERSION --output target/IRI-$1-SHA256SUM
+      - curl -s https://iotaledger-dbfiles-public.s3.eu-central-1.amazonaws.com/mainnet/iri/latest-LS.tar --output target/LS-\\\$(cat /cache/milestone.txt).tar
+      - curl -s https://iotaledger-dbfiles-public.s3.eu-central-1.amazonaws.com/mainnet/iri/latest-LS.tar.sum --output target/LS-\\\$(cat /cache/milestone.txt)-SHA256SUM
+      - if [[ \\\$(sha256sum target/iri-$1.jar | cut -d \" \" -f 1) == \\\$(cat target/SHA256SUM-$1) ]]; then echo 'CHECKSUM OK'; else exit 1; fi
+      - if [[ \\\$(sha256sum target/local-snapshot.tar | cut -d \" \" -f 1) == \\\$(cat target/local-snapshot.tar.sum) ]]; then echo 'CHECKSUM OK'; else exit 1; fi
       - curl -L https://github.com/buildkite/github-release/releases/download/v1.0/github-release-linux-amd64 -o github-release
       - chmod +x github-release      
       - echo \\\$GPG_KEY | base64 -d > iri.key
       - echo \\\$GPG_CONTACT_PASSPHRASE | gpg --batch --yes --import <iri.key
-      - echo \\\$GPG_CONTACT_PASSPHRASE | gpg --pinentry-mode loopback --batch --passphrase-fd 0 --armor --detach-sign --default-key contact@iota.org target/SHA256SUM-\\\$IRI_VERSION
+      - echo \\\$GPG_CONTACT_PASSPHRASE | gpg --pinentry-mode loopback --batch --passphrase-fd 0 --armor --detach-sign --default-key contact@iota.org target/IRI-$1-SHA256SUM
+      - echo \\\$GPG_CONTACT_PASSPHRASE | gpg --pinentry-mode loopback --batch --passphrase-fd 0 --armor --detach-sign --default-key contact@iota.org target/LS-\\\$(cat /cache/milestone.txt)-SHA256SUM
       - ./github-release \\\$GITHUB_RELEASE_TAG target/*"
   echo "    plugins:
       https://github.com/iotaledger/docker-buildkite-plugin#release-v3.2.0:
@@ -39,7 +78,9 @@ release () {
           - GITHUB_RELEASE_REPOSITORY=sadjy/iri
           - GITHUB_RELEASE_COMMIT
           - GPG_CONTACT_PASSPHRASE
-          - GPG_KEY"
+          - GPG_KEY
+        volumes:
+          - /cache-iri-release-$BUILDKITE_BUILD_ID:/cache"
   echo "    agents:
       queue: aws-m5large"
 }
@@ -59,7 +100,8 @@ docker_push () {
         always-pull: true
         mount-buildkite-agent: false
         volumes:
-        - /var/run/docker.sock:/var/run/docker.sock
+          - /var/run/docker.sock:/var/run/docker.sock
+          - /cache-iri-release-$BUILDKITE_BUILD_ID:/cache
         environment:
           - DOCKER_USERNAME
           - DOCKER_PASSWORD"
@@ -70,7 +112,8 @@ docker_push () {
 echo "steps:"
 GIT_TAG=$(git describe --exact-match --tags HEAD || true)
 if [[ "$GIT_TAG" == *"RELEASE" ]]; then
-  release "$GIT_TAG"
+  getMilestone "$(echo $GIT_TAG | tr -d 'v')"
+  release "$(echo $GIT_TAG | tr -d 'v')"
   docker_push "$GIT_TAG"
 else
   skip_build
